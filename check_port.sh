b02383cb2346229580e8d35bfcd5f9e67617fdc0
Roman <kolmeny1@gmail.com>
2021-09-20 16:27:20 +0300

    initial commit

---
+++ check_port.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+@echo off
+while :
+do
+    while IFS= read -r line; do
+        IFS=':' read -ra ip_port <<< $line
+        echo ip - ${ip_port[0]} port - ${ip_port[1]}   
+        result=$(nc -zv -w3 ${ip_port[0]} ${ip_port[1]} 2>&1 | grep succeeded) 
+        if ["$result" = ""]; then
+            echo port ${ip_port[1]} is closed on ${ip_port[0]}
+            curl -X POST -H "Content-Type: application/json" -d '{"failed": "${ip_port[0]}:${ip_port[1]}",}' https://example/contact
+        else
+            echo port ${ip_port[1]} is open on ${ip_port[0]}
+        fi
+    done < test.txt
+    sleep 60
+done
